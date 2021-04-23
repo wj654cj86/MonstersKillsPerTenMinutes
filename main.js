c018fb85950fca95d549d5057a00a0c751e6c0b9
WilliamTu <wj654cj86@gmail.com>
2021-04-23 16:16:59 +0800

    V1.0.0 初始版本

---
+++ main.js
@@ -0,0 +1,222 @@
+function createth(str, style, width, cb) {
+	let th = document.createElement('th');
+	th.innerHTML = str;
+	if (typeof style != 'undefined')
+		th.className = style;
+	if (typeof width != 'undefined')
+		th.style.width = width + 'px';
+	if (typeof cb != 'undefined')
+		th.onclick = cb;
+	return th;
+}
+
+function createtd(str, style) {
+	let td = document.createElement('td');
+	td.innerHTML = str;
+	if (typeof style != 'undefined')
+		td.className = style;
+	return td;
+}
+
+var job = {
+	'劍士': ['英雄', '聖騎士', '黑騎士', '聖魂劍士', '米哈逸', '狂狼勇士', '爆拳槍神', '惡魔殺手', '惡魔復仇者', '凱薩', '神之子', '阿戴爾', '劍豪', '皮卡啾'],
+	'法師': ['火毒', '冰雷', '主教', '烈焰巫師', '龍魔導士', '夜光', '煉獄巫師', '凱內西斯', '伊利恩', '陰陽師', '幻獸師'],
+	'弓箭手': ['箭神', '神射手', '開拓者', '破風使者', '精靈遊俠', '狂豹獵人', '卡音'],
+	'盜賊': ['夜使者', '暗影神偷', '影武者', '暗夜行者', '幻影俠盜', '傑諾', '卡蒂娜', '虎影'],
+	'海盜': ['拳霸', '槍神', '重砲指揮官', '閃雷悍將', '隱月', '機甲戰神', '傑諾', '天使破壞者', '亞克', '墨玄', '雪吉拉', '傑特']
+};
+
+function 切換職業(jobname) {
+	職業.innerHTML = '';
+	if (jobname in job) {
+		let jobls = job[jobname];
+		for (let val of jobls) {
+			let op = document.createElement('option');
+			op.value = val;
+			op.innerHTML = val;
+			職業.append(op);
+		}
+	}
+}
+
+var url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSplDOktwi4lBhEY3JqBbs5tdF4MrX_wBJ4u28c6IiO8xnhWPOc2FeSVBr7aNlfg4fVzdORQQ-qX0K-/pubhtml';
+var 資料表 = [];
+var 地圖經驗表 = [];
+var 沒有資料列 = (() => {
+	let tr = document.createElement('tr');
+	let td = document.createElement('td');
+	td.colSpan = 10;
+	td.innerHTML = '暫無資料';
+	td.className = '偶數行';
+	tr.append(td);
+	return tr;
+})();
+
+var 顯示 = [];
+var 怪物隻數隱藏表 = document.createElement('tbody');
+function 列出怪物隻數(jobname) {
+	顯示 = [];
+	for (let i = 0; i < 資料表.length; i++) {
+		if (資料表[i].職業 == jobname) {
+			怪物隻數表.append(資料表[i].html);
+			顯示.push(資料表[i]);
+		} else {
+			怪物隻數隱藏表.append(資料表[i].html);
+		}
+	}
+	if (顯示.length == 0) {
+		怪物隻數表.append(沒有資料列);
+	} else {
+		怪物隻數隱藏表.append(沒有資料列);
+	}
+	奇偶數行顏色設定();
+}
+
+function 奇偶數行顏色設定() {
+	for (let i = 0; i < 顯示.length; i++) {
+		let r = 顯示[i].html;
+		if (i & 1) {
+			r.className = '奇數行';
+		} else {
+			r.className = '偶數行';
+		}
+	}
+}
+
+function 重新顯示() {
+	for (let i = 0; i < 顯示.length; i++) {
+		怪物隻數表.append(顯示[i].html);
+	}
+	奇偶數行顏色設定();
+}
+
+let 順序 = {
+	'地圖': true,
+	'擊殺數': true,
+	'經驗值': true
+};
+
+function 排序(key) {
+	順序[key] = !順序[key];
+	if (順序[key]) {
+		顯示.sort((a, b) => {
+			if (a[key] < b[key])
+				return -1;
+			else if (a[key] > b[key])
+				return 1;
+			else
+				return 0;
+		});
+	} else {
+		顯示.sort((a, b) => {
+			if (a[key] < b[key])
+				return 1;
+			else if (a[key] > b[key])
+				return -1;
+			else
+				return 0;
+		});
+	}
+	重新顯示();
+}
+
+window.onload = async () => {
+	for (let key in job) {
+		let op = document.createElement('option');
+		op.value = key;
+		op.innerHTML = key;
+		職業群.append(op);
+	}
+	切換職業(職業群.value);
+
+	職業群.onchange = () => {
+		切換職業(職業群.value);
+		列出怪物隻數(職業.value);
+	};
+	職業.onchange = () => {
+		列出怪物隻數(職業.value);
+	};
+
+	let htmlstr = await promise(openfile, url);
+	let loadtrls = (n) => {
+		let tbodybegin, tbodyend = 0;
+		for (let i = 0; i < n; i++) {
+			tbodyend = htmlstr.indexOf('</tbody>', tbodyend) + '</tbody>'.length;
+		}
+		tbodybegin = htmlstr.indexOf('<tbody>', tbodyend);
+		tbodyend = htmlstr.indexOf('</tbody>', tbodyend) + '</tbody>'.length;
+		return text2xml(htmlstr.slice(tbodybegin, tbodyend)).getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+	};
+	let trls = loadtrls(1);
+
+	for (let i = 0; i < trls.length; i++) {
+		let tdls = trls[i].getElementsByTagName('td');
+		地圖經驗表[i] = {
+			'地圖': tdls[0].innerHTML,
+			'經驗': tdls[1].innerHTML
+		};
+	}
+	// console.log(JSON.stringify(地圖經驗表));
+
+	let tr = document.createElement('tr');
+	tr.append(createth('樓層', null, 60));
+	tr.append(createth('職業', null, 100));
+	tr.append(createth('地圖', null, 150, () => {
+		排序('地圖');
+	}));
+	tr.append(createth('擊殺數', null, 100, () => {
+		排序('擊殺數');
+	}));
+	tr.append(createth('經驗值', null, 100, () => {
+		排序('經驗值');
+	}));
+	tr.append(createth('幽暗', null, 60));
+	tr.append(createth('影片', null, 60));
+	tr.append(createth('測試者', null, 150));
+	tr.append(createth('備註', null, 200));
+	tr.append(createth('版本', null, 200));
+	怪物隻數表thead.append(tr);
+
+	trls = loadtrls(0);
+	for (let i = 0; i < trls.length; i++) {
+		let tdls = trls[i].getElementsByTagName('td');
+		資料表[i] = {
+			'樓層': tdls[0].innerHTML,
+			'職業': tdls[1].innerHTML,
+			'地圖': tdls[2].innerHTML,
+			'擊殺數': tdls[3].innerHTML * 1,
+			'幽暗': tdls[4].innerHTML,
+			'影片': tdls[5].innerHTML,
+			'測試者': tdls[6].innerHTML,
+			'備註': tdls[7].innerHTML,
+			'版本': tdls[8].innerHTML
+		};
+
+		let ef = 地圖經驗表.find((e) => {
+			return e.地圖 == 資料表[i].地圖;
+		});
+
+		if (typeof ef == 'undefined')
+			資料表[i].經驗值 = '該地圖未有平均經驗'
+		else {
+			資料表[i].經驗值 = (ef.經驗 * 資料表[i].擊殺數 / 1E8).toFixed(2) * 1;
+		}
+
+		let r = 資料表[i];
+		let tr = document.createElement('tr');
+		tr.append(createtd(r.樓層));
+		tr.append(createtd(r.職業));
+		tr.append(createtd(r.地圖));
+		tr.append(createtd(r.擊殺數));
+		tr.append(createtd(typeof r.經驗值 == 'number' ? r.經驗值.toFixed(2) + '億' : r.經驗值));
+		tr.append(createtd(r.幽暗));
+		tr.append(createtd(r.影片));
+		tr.append(createtd(r.測試者));
+		tr.append(createtd(r.備註));
+		tr.append(createtd(r.版本));
+		r.html = tr;
+	}
+	// console.log(JSON.stringify(資料表));
+
+	列出怪物隻數(職業.value);
+};
---
+++ mylibrary.js
@@ -0,0 +1,291 @@
+function paddingLeft(str, lenght) {
+	str += '';
+	if (str.length >= lenght)
+		return str;
+	else
+		return paddingLeft("0" + str, lenght);
+}
+
+function setCookie(cname, cvalue) {
+	var d;
+	var expires = '';
+	d = new Date();
+	d.setTime(d.getTime() + (100 * 24 * 60 * 60 * 1000));
+	expires = "expires=" + d.toUTCString() + ";";
+	document.cookie = cname + "=" + cvalue + ";" + expires + "path=" + location.pathname;
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function url2array() {
+	var arr = [];
+	var strUrl = location.search;
+	if (strUrl.indexOf('?') != -1) {
+		var allData = strUrl.split("?")[1].split("&");
+		for (var i = 0; i < allData.length; i++) {
+			var data = allData[i].split("=");
+			arr[data[0]] = decodeURIComponent(data[1]);
+		}
+	}
+	return arr;
+}
+
+function array2url(arr) {
+	var allData = [];
+	for (var i in arr) {
+		allData.push(i + '=' + encodeURIComponent(arr[i]));
+	}
+	var strUrl = allData.length != 0 ? ('?' + allData.join('&')) : '';
+	var url = location.href.split('?')[0];
+	window.history.pushState({}, 0, url + strUrl + location.hash);
+}
+
+function promise(callback, ...args) {
+	return new Promise((resolve, reject) => {
+		callback(...args, (data) => {
+			resolve(data);
+		});
+	});
+}
+
+function promisearr(callback, ...args) {
+	return new Promise((resolve, reject) => {
+		callback(...args, (...args) => {
+			resolve(args);
+		});
+	});
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function openfile(url, callback) {
+	if (typeof callback == "undefined") {
+		callback = function (str) { };
+	}
+	let oReq = new XMLHttpRequest();
+	oReq.addEventListener("load", function () {
+		if (oReq.status != 404) {
+			callback(this.responseText);
+		} else {
+			callback('{}');
+		}
+	});
+	oReq.addEventListener("error", function () {
+		callback('{}');
+	});
+	oReq.open("GET", url);
+	oReq.send();
+}
+
+function text2xml(text) {
+	let parser = new DOMParser();
+	return parser.parseFromString(text, "text/xml");
+}
+
+function xml2text(xml) {
+	let xsl = new XMLSerializer();
+	return xsl.serializeToString(xml);
+}
+
+function copyxml(xml) {
+	return text2xml(xml2text(xml));
+}
+
+function getimgsize(imgsrc, callback) {
+	let a = new Image();
+	a.onload = function () {
+		callback(a.naturalWidth, a.naturalHeight);
+	};
+	a.onerror = function () {
+		callback(-1, -1);
+	};
+	a.src = imgsrc;
+}
+
+function loadimg(imgsrc, callback) {
+	let img = new Image();
+	img.onload = function () {
+		callback(img);
+	};
+	img.src = imgsrc;
+}
+
+function loadsound(src, callback) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', src);
+	xhr.responseType = "blob";
+	xhr.send();
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			let blob = this.response;
+			callback(URL.createObjectURL(blob));
+		}
+	}
+}
+
+function svgtoimg(svg, callback) {
+	let svgstring = xml2text(svg);
+	let img = new Image();
+	let blob = new Blob([svgstring], { type: 'image/svg+xml' });
+	let url = URL.createObjectURL(blob);
+	img.onload = function () {
+		callback();
+	};
+	img.src = url;
+	return img;
+}
+
+function svgtopngurl(svg, callback) {
+	let img = svgtoimg(svg, function () {
+		let c = document.createElement("canvas");
+		c.setAttribute('width', img.naturalWidth);
+		c.setAttribute('height', img.naturalHeight);
+		let ctx = c.getContext("2d");
+		ctx.drawImage(img, 0, 0);
+		c.toBlob(function (blob) {
+			let url = URL.createObjectURL(blob);
+			callback(url);
+		});
+	});
+}
+
+function pngtobase64(imgsrc, callback) {
+	let img = new Image();
+	img.onload = function () {
+		let c = document.createElement("canvas");
+		c.setAttribute('width', img.naturalWidth);
+		c.setAttribute('height', img.naturalHeight);
+		let ctx = c.getContext("2d");
+		ctx.drawImage(img, 0, 0);
+		callback(c.toDataURL());
+	};
+	img.src = imgsrc;
+}
+
+function getclickpoint(event, element) {
+	return {
+		x: event.clientX - element.offsetLeft + document.documentElement.scrollLeft + document.body.scrollLeft,
+		y: event.clientY - element.offsetTop + document.documentElement.scrollTop + document.body.scrollTop
+	};
+}
+
+function getCursorPosition(event) {
+	let posx = 0;
+	let posy = 0;
+	if (!event) event = window.event;
+	if (event.pageX || event.pageY) {
+		posx = event.pageX - document.documentElement.scrollLeft - document.body.scrollLeft;
+		posy = event.pageY - document.documentElement.scrollTop - document.body.scrollTop;
+	} else if (event.clientX || event.clientY) {
+		posx = event.clientX;
+		posy = event.clientY;
+	}
+	return { x: posx, y: posy };
+}
+
+function startDownload(url, name) {
+	let download = document.createElement('a');
+	download.href = url;
+	download.download = name;
+	download.click();
+}
+
+function arrsum(arr) {
+	let sum = 0;
+	let len = arr.length;
+	for (let i = 0; i < len; i++) {
+		sum += arr[i] * 1;
+	}
+	return sum;
+}
+function arraverage(arr) {
+	let len = arr.length;
+	if (len != 0)
+		return arrsum(arr) / len;
+	else
+		return 0;
+}
+function arrsd(arr) {
+	let sum = 0;
+	let average = arraverage(arr);
+	let len = arr.length;
+	for (let i = 0; i < len; i++) {
+		let k = arr[i] - average;
+		sum += k * k;
+	}
+	return Math.sqrt(sum / len);
+}
+
+function componentToHex(c) {
+	c = Math.floor(c * 1);
+	var hex = c.toString(16);
+	return paddingLeft(hex, 2);
+}
+
+function rgbToHex(r, g, b) {
+	return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function hexToRgb(h) {
+	let r, g, b;
+	if (h.length == 4) {
+		r = 0x11 * ('0x' + h[1]);
+		g = 0x11 * ('0x' + h[2]);
+		b = 0x11 * ('0x' + h[3]);
+	} else {
+		r = 1 * ('0x' + h[1] + h[2]);
+		g = 1 * ('0x' + h[3] + h[4]);
+		b = 1 * ('0x' + h[5] + h[6]);
+	}
+	return [r, g, b];
+}
+
+Node.prototype.getElementsByAttributeValue = function (attribute, value) {
+	var dom = this.all || this.getElementsByTagName("*");
+	var match = new Array();
+	for (var i in dom) {
+		if ((typeof dom[i]) === "object") {
+			if (dom[i].getAttribute(attribute) == value) {
+				match.push(dom[i]);
+			}
+		}
+	}
+	return match;
+};
+
+Node.prototype.getElementByIdSvg = function (value) {
+	return this.getElementsByAttributeValue('id', value)[0];
+};
+
+function removeChild(node) {
+	if (node.parentNode) {
+		node.parentNode.removeChild(node);
+	}
+}
+
+function sentpost(url, obj) {
+	obj = obj || {};
+
+	let oReq = new XMLHttpRequest();
+	oReq.open("POST", url, true);
+	oReq.setRequestHeader('Content-Type', 'application/json');
+	oReq.onreadystatechange = function () {
+	};
+	oReq.send(JSON.stringify(obj));
+}
