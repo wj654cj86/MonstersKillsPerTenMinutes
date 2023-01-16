97dd6b72b83e785b64dd59eca3f7400215bb2029
WilliamTu <wj654cj86@gmail.com>
2023-01-16 17:54:32 +0800

    V1.6.0 減少使用for in 新增滑鼠移動到表格時會額外顯示內容以防止超出內容無法觀看 修正排序無法排字串問題 將版號、版本分開 優化css

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 let url = 'https://script.google.com/macros/s/AKfycbyyRfcTGy6wZPm5AWonzjdYwW1b1w-uvTnNvjs-GsRIyFSKqtJTB5Uz18fTnUkS8IUm/exec';
 let 巴哈討論串 = 'https://forum.gamer.com.tw/C.php?bsn=7650&snA=995533&to=';
 
-let creatediv = str => text2html(`<td><div>${str}</div></td>`);
-let createa = str => text2html(`<td><div><a href="${巴哈討論串 + str}">移至${str}樓</a></div></td>`);
+let creatediv = str => text2html(`<td><div title="${str}">${str}</div></td>`);
+let createa = str => text2html(`<td><div><a title="移至${str}樓" href="${巴哈討論串 + str}">移至${str}樓</a></div></td>`);
 let createop = str => text2html(`<option value="${str}">${str}</option>`);
-let createlongtd = str => text2html(`<tr><td colspan="13">${str}</td></tr>`);
+let createlongtd = str => text2html(`<tr><td colspan="14">${str}</td></tr>`);
+
+Object.prototype.entries = function () { return Object.entries(this); };
+Object.prototype.forEach = function (cb = () => { }) { this.entries().forEach(([k, v]) => cb(v, k)); };
 
 function createshow(obj, name) {
-	let div = creatediv('');
-	Object.defineProperty(obj, name, {
-		set: function (str) {
-			div.innerHTML = str;
-		}
-	});
-	return div;
+	let td = creatediv('');
+	let div = td.querySelector('div');
+	Object.defineProperty(obj, name, { set: str => div.title = div.innerHTML = str });
+	return td;
 }
 
 let 職業表 = [];
@@ -24,15 +24,15 @@ function 切換職業() {
 	let 類型名稱 = 類型.value;
 	let 群體名稱 = 群體.value;
 	顯示職業 = [];
-	for (let i = 0; i < 職業表.length; i++) {
-		if (職業表[i].職業 == '全部顯示' || ((類型名稱 == '全部' || 職業表[i].類型 == 類型名稱 || (職業表[i].類型 == '傑諾' && (類型名稱 == '盜賊' || 類型名稱 == '海盜')))
-			&& (群體名稱 == '全部' || 職業表[i].群體 == 群體名稱))) {
-			職業.append(職業表[i].html);
-			顯示職業.push(職業表[i]);
+	職業表.forEach(v => {
+		if (v.職業 == '全部顯示' || ((類型名稱 == '全部' || v.類型 == 類型名稱 || (v.類型 == '傑諾' && (類型名稱 == '盜賊' || 類型名稱 == '海盜')))
+			&& (群體名稱 == '全部' || v.群體 == 群體名稱))) {
+			職業.append(v.html);
+			顯示職業.push(v);
 		} else {
-			職業隱藏.append(職業表[i].html);
+			職業隱藏.append(v.html);
 		}
-	}
+	});
 	if (顯示職業.length == 1) {
 		職業.append(沒有符合職業.html);
 		顯示職業 = [沒有符合職業];
@@ -55,27 +55,21 @@ function 列出怪物隻數() {
 	let 職業名稱 = 職業.value;
 	let 區域名稱 = 區域.value;
 	顯示 = [];
-	for (let i = 0; i < 資料表.length; i++) {
-		if ((資料表[i].職業 == 職業名稱 || (職業名稱 == '全部顯示' && (() => {
-			for (let v of 顯示職業)
-				if (資料表[i].職業 == v.職業) return true;
-			return false;
-		})())) && (區域名稱 == '全部' || 區域名稱 == 資料表[i].區域)) {
-			怪物隻數表.append(資料表[i].html);
-			顯示.push(資料表[i]);
+	資料表.forEach(v => {
+		if ((v.職業 == 職業名稱 || (職業名稱 == '全部顯示' && 顯示職業.find(e => e.職業 == v.職業) !== undefined))
+			&& (區域名稱 == '全部' || 區域名稱 == v.區域)) {
+			怪物隻數表.append(v.html);
+			顯示.push(v);
 		} else {
-			怪物隻數隱藏表.append(資料表[i].html);
+			怪物隻數隱藏表.append(v.html);
 		}
-	}
+	});
 	if (顯示.length == 0) {
 		怪物隻數表.append(沒有資料列);
 	} else {
 		怪物隻數隱藏表.append(沒有資料列);
 	}
-	for (let k in 按鈕) {
-		按鈕[k].innerHTML = '▲';
-		順序[k] = true;
-	}
+	順序.forEach((v, k) => 順序[k] = true);
 	顯示經驗與楓幣();
 }
 
@@ -105,9 +99,7 @@ function 顯示經驗與楓幣() {
 
 function 顯示擊殺總值() {
 	let 時間倍率 = (時間小時.value * 60 + 時間分鐘.value * 1) / 10;
-	for (let i = 0; i < 顯示.length; i++) {
-		顯示[i].擊殺數顯示 = 顯示[i].時間擊殺數 = (顯示[i].擊殺數 * 時間倍率).toFixed(0);
-	}
+	顯示.forEach((v, i) => v.擊殺數顯示 = v.時間擊殺數 = (v.擊殺數 * 時間倍率).toFixed(0));
 }
 
 let 等差經驗增量 = [];
@@ -119,32 +111,21 @@ function 顯示經驗總值() {
 		return;
 	}
 	經驗量錯誤.innerHTML = '';
-	for (let i = 0; i < 顯示.length; i++) {
-		if (顯示[i].地圖經驗 === undefined) {
-			顯示[i].經驗值顯示 = '未有地圖經驗';
-			顯示[i].經驗值 = 0;
-			continue;
+	顯示.forEach(v => {
+		if (v.地圖經驗 === undefined) {
+			v.經驗值顯示 = '未有地圖經驗';
+			v.經驗值 = 0;
+			return;
 		}
-		let 等級差 = 等級 - 顯示[i].地圖經驗.A怪.等級;
-		let j;
-		for (j = 0; j < 等差經驗增量.length; j++) {
-			if (等級差 >= 等差經驗增量[j].等級) {
-				break;
-			}
+		let 等級差 = 等級 - v.地圖經驗.A怪.等級;
+		let 經驗 = v.地圖經驗.A怪.經驗 * 等差經驗增量.find(e => 等級差 >= e.等級).經驗;
+		if (v.地圖經驗.B怪 !== undefined) {
+			等級差 = 等級 - v.地圖經驗.B怪.等級;
+			經驗 = (經驗 + v.地圖經驗.B怪.經驗 * 等差經驗增量.find(e => 等級差 >= e.等級).經驗) / 2;
 		}
-		let 經驗 = 顯示[i].地圖經驗.A怪.經驗 * 等差經驗增量[j].經驗;
-		if (顯示[i].地圖經驗.B怪 !== undefined) {
-			等級差 = 等級 - 顯示[i].地圖經驗.B怪.等級;
-			for (j = 0; j < 等差經驗增量.length; j++) {
-				if (等級差 >= 等差經驗增量[j].等級) {
-					break;
-				}
-			}
-			經驗 = (經驗 + 顯示[i].地圖經驗.B怪.經驗 * 等差經驗增量[j].經驗) / 2;
-		}
-		顯示[i].經驗值 = 經驗 * 顯示[i].時間擊殺數 * (1 + 經驗百分比 / 100) / 1E8;
-		顯示[i].經驗值顯示 = (顯示[i].經驗值).toFixed(2) + '億';
-	}
+		v.經驗值 = 經驗 * v.時間擊殺數 * (1 + 經驗百分比 / 100) / 1E8;
+		v.經驗值顯示 = (v.經驗值).toFixed(2) + '億';
+	});
 }
 
 let 等差楓幣增量 = [];
@@ -165,80 +146,54 @@ function 顯示楓幣總值() {
 		return;
 	}
 	楓幣量錯誤.innerHTML = '';
-
-	for (let i = 0; i < 顯示.length; i++) {
-		if (顯示[i].地圖經驗 === undefined) {
-			顯示[i].楓幣量顯示 = '未有怪物等級';
-			顯示[i].楓幣量 = 0;
-			continue;
+	顯示.forEach(v => {
+		if (v.地圖經驗 === undefined) {
+			v.楓幣量顯示 = '未有怪物等級';
+			v.楓幣量 = 0;
+			return;
 		}
-		let 等級差 = 等級 - 顯示[i].地圖經驗.A怪.等級;
-		let j;
-		for (j = 0; j < 等差楓幣增量.length; j++) {
-			if (等級差 >= 等差楓幣增量[j].等級) {
-				break;
-			}
+		let 等級差 = 等級 - v.地圖經驗.A怪.等級;
+		let 楓幣 = 7.5 * v.地圖經驗.A怪.等級 * 等差楓幣增量.find(e => 等級差 >= e.等級).楓幣;
+		if (v.地圖經驗.B怪 !== undefined) {
+			等級差 = 等級 - v.地圖經驗.B怪.等級;
+			楓幣 = (楓幣 + 7.5 * v.地圖經驗.B怪.等級 * 等差楓幣增量.find(e => 等級差 >= e.等級).楓幣) / 2;
 		}
-		let 楓幣 = 7.5 * 顯示[i].地圖經驗.A怪.等級 * 等差楓幣增量[j].楓幣;
-		if (顯示[i].地圖經驗.B怪 !== undefined) {
-			等級差 = 等級 - 顯示[i].地圖經驗.B怪.等級;
-			for (j = 0; j < 等差楓幣增量.length; j++) {
-				if (等級差 >= 等差楓幣增量[j].等級) {
-					break;
-				}
-			}
-			楓幣 = (楓幣 + 7.5 * 顯示[i].地圖經驗.B怪.等級 * 等差楓幣增量[j].楓幣) / 2;
-		}
-		顯示[i].楓幣量 = 楓幣 * 實際掉寶率 * (1 + 楓幣百分比 / 100) * 顯示[i].時間擊殺數 / 1E4;
-		顯示[i].楓幣量顯示 = (顯示[i].楓幣量).toFixed(0) + '萬';
-	}
-}
-
-function 重新顯示() {
-	for (let i = 0; i < 顯示.length; i++) {
-		怪物隻數表.append(顯示[i].html);
-	}
+		v.楓幣量 = 楓幣 * 實際掉寶率 * (1 + 楓幣百分比 / 100) * v.時間擊殺數 / 1E4;
+		v.楓幣量顯示 = (v.楓幣量).toFixed(0) + '萬';
+	});
 }
 
 let 順序 = {};
-let 按鈕 = {};
 function createth(name, 排序按鈕) {
 	let th = text2html(`<th>${name}</th>`);
 	if (排序按鈕 === true) {
+		let bb = true;
 		let btn = text2html(`<button>▲</button>`);
 		btn.onclick = () => 排序(name);
-		按鈕[name] = btn;
-		順序[name] = true;
+		Object.defineProperty(順序, name, {
+			set: b => btn.innerHTML = (bb = b) ? '▲' : '▼',
+			get: () => bb,
+			enumerable: true
+		});
 		th.append(' ', btn);
 	}
 	return th;
 }
 
 function 排序(key) {
-	順序[key] = !順序[key];
-	for (let k in 按鈕) {
-		if (k == key) continue;
-		按鈕[k].innerHTML = '▲';
-		順序[k] = true;
-	}
+	順序.forEach((v, k) => 順序[k] = k == key ? !順序[k] : true);
 	if (順序[key]) {
-		按鈕[key].innerHTML = '▲';
-		顯示.sort((a, b) => a[key] - b[key]);
+		顯示.sort((a, b) => a[key] == b[key] ? 0 : a[key] > b[key] ? 1 : -1);
 	} else {
-		按鈕[key].innerHTML = '▼';
-		顯示.sort((a, b) => b[key] - a[key]);
+		顯示.sort((a, b) => b[key] == a[key] ? 0 : b[key] > a[key] ? 1 : -1);
 	}
-	重新顯示();
+	顯示.forEach(e => 怪物隻數表.append(e.html));
 }
 
 function 表格寬度設定() {
-	let 寬度 = [70, 90, 120, 200, 100, 100, 100, 40, 40, 150, 170, 200, 100];
+	let 寬度 = [50, 100, 120, 200, 100, 100, 100, 40, 40, 150, 150, 50, 150, 100];
 	let outstr = '';
-	for (let i = 0; i < 寬度.length; i++) {
-		outstr += `#主要表 table th:nth-child(${i + 1}),`
-			+ `#主要表 table tr td:nth-child(${i + 1}) div`
-			+ `{width:${寬度[i]}px;}\n`;
-	}
+	寬度.forEach((v, i) => outstr += `#主要表 table th:nth-child(${i + 1}),#主要表 table tr td:nth-child(${i + 1}) div{width:${v}px;}\n`);
 	document.head.append(text2html(`<style>${outstr}</style>`));
 }
 
@@ -267,6 +222,7 @@ window.onload = async () => {
 	tr.append(createth('影片'));
 	tr.append(createth('測試者'));
 	tr.append(createth('備註'));
+	tr.append(createth('版號'));
 	tr.append(createth('版本'));
 	tr.append(createth('巴哈連結'));
 	怪物隻數表thead.append(tr);
@@ -275,11 +231,9 @@ window.onload = async () => {
 	let obj = await loadfile('json', url);
 	// console.log(obj);
 
-	let page = obj[3];
 	let 類型表 = {};
 	let 群體表 = {};
-	for (let i = 0; i < page.length; i++) {
-		let row = page[i];
+	obj[3].forEach((row, i) => {
 		職業表[i] = {
 			'職業': row[0],
 			'類型': row[1],
@@ -289,24 +243,22 @@ window.onload = async () => {
 		職業表[i].html = createop(職業表[i].職業);
 		類型表[職業表[i].類型] = true;
 		群體表[職業表[i].群體] = true;
-	}
-	// console.log(JSON.stringify(職業表));
-	// console.log(JSON.stringify(類型表));
-	// console.log(JSON.stringify(群體表));
-	for (let key in 類型表) {
+	});
+	// console.log(職業表);
+	// console.log(類型表);
+	// console.log(群體表);
+	類型表.forEach((v, key) => {
 		if (key == '類型') key = '全部';
-		if (key == '傑諾') continue;
+		if (key == '傑諾') return;
 		類型.append(createop(key));
-	}
-	for (let key in 群體表) {
+	});
+	群體表.forEach((v, key) => {
 		if (key == '群體') key = '全部';
 		群體.append(createop(key));
-	}
+	});
 
-	page = obj[2];
 	let 區域表 = {};
-	for (let i = 0; i < page.length; i++) {
-		let row = page[i];
+	obj[2].forEach((row, i) => {
 		地圖經驗表[i] = {
 			'區域': row[0],
 			'地圖': row[1],
@@ -322,58 +274,50 @@ window.onload = async () => {
 			}
 		}
 		區域表[地圖經驗表[i].區域] = true;
-	}
+	});
 	地圖經驗表.shift();
-	// console.log(JSON.stringify(地圖經驗表));
-	for (let key in 區域表) {
+	// console.log(地圖經驗表);
+	// console.log(區域表);
+	區域表.forEach((v, key) => {
 		if (key == '區域') key = '全部';
 		區域.append(createop(key));
-	}
+	});
 
-	page = obj[4];
-	for (let i = 0; i < page.length; i++) {
-		let row = page[i];
-		等差經驗增量[i] = {
+	等差經驗增量 = obj[4].map(row => {
+		return {
 			'等級': row[0],
 			'經驗': row[1]
-		};
-	}
+		}
+	});
 	等差經驗增量.shift();
+	// console.log(等差經驗增量);
 
-	page = obj[1];
-	for (let i = 0; i < page.length; i++) {
-		let row = page[i];
-		等差楓幣增量[i] = {
+	等差楓幣增量 = obj[1].map(row => {
+		return {
 			'等級': row[0],
 			'楓幣': row[1]
-		};
-	}
+		}
+	});
 	等差楓幣增量.shift();
+	// console.log(等差楓幣增量);
 
-	page = obj[0];
-	for (let i = 0; i < page.length; i++) {
-		let row = page[i];
-		資料表[i] = {
+	obj[0].forEach((row, i) => {
+		let r = 資料表[i] = {
 			'樓層': row[0],
 			'職業': row[1],
 			'地圖': row[2],
-			'擊殺數': row[3] * 1,
+			'擊殺數': row[3],
 			'幽暗': row[4],
 			'影片': row[5],
 			'測試者': row[6],
-			'備註': row[7],
-			'版本': row[8]
+			'備註': row[7]
 		};
+		r.地圖經驗 = 地圖經驗表.find(e => e.地圖 == r.地圖);
+		r.區域 = r.地圖經驗 === undefined ? '資料庫未設置區域' : r.地圖經驗.區域;
+		let 版本說明 = row[8].replace(/ \/ /g, '/').split('/');
+		r.版號 = 版本說明[0];
+		r.版本 = 版本說明.length == 2 ? 版本說明[1] : '';
 
-		資料表[i].地圖經驗 = 地圖經驗表.find((e) => {
-			return e.地圖 == 資料表[i].地圖;
-		});
-
-		let r = 資料表[i];
-		if (r.地圖經驗 === undefined)
-			r.區域 = '資料庫未設置區域';
-		else
-			r.區域 = r.地圖經驗.區域;
 		let tr = document.createElement('tr');
 		tr.append(creatediv(r.樓層));
 		tr.append(creatediv(r.職業));
@@ -386,12 +330,13 @@ window.onload = async () => {
 		tr.append(creatediv(r.影片));
 		tr.append(creatediv(r.測試者));
 		tr.append(creatediv(r.備註));
+		tr.append(creatediv(r.版號));
 		tr.append(creatediv(r.版本));
 		tr.append(createa(r.樓層));
 		r.html = tr;
-	}
+	});
 	資料表.shift();
-	// console.log(JSON.stringify(資料表));
+	// console.log(資料表);
 
 	怪物隻數隱藏表.append(資料載入中);
 	切換職業();
